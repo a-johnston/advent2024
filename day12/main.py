e942fa3620628fb2660c53b82618bbe39e8b8cd6
Adam Johnston <adam.johnston00@gmail.com>
2024-12-12 14:34:36 -0800

    simplify

--- day12/main.py
+++ day12/main.py
@@ -1,5 +1,4 @@
 from collections import Counter
-from dataclasses import dataclass
 
 from adventlib import grid, vector
 
@@ -10,76 +9,50 @@ side_checks = (
     (grid.east, grid.north),
 )
 
-@dataclass
-class Group:
-    index: int
-    members: set
+def border(group):
+    total = 0
+    for member in group:
+        for d in grid.cardinals:
+            if vector.add(member, d) not in group:
+                total += 1
+    return total
 
-    @property
-    def area(self):
-        return len(self.members)
-
-    @property
-    def border(self):
-        total = 0
-        for member in self.members:
-            for d in grid.cardinals:
-                if vector.add(member, d) not in self.members:
-                    total += 1
-        return total
-
-    @property
-    def sides(self):
-        total = 0
-        for member in self.members:
-            for a, b in side_checks:
-                if (vector.add(member, a) not in self.members
-                    and (vector.add(member, b) not in self.members
-                         or vector.add(member, b, a) in self.members)):
-                    total += 1
-        return total
-
-    def merge(self, groups, other):
-        self.members |= other.members
-        for index in groups:
-            if groups[index] == other:
-                groups[index] = self
-
-    def __hash__(self):
-        return self.index
+def sides(group):
+    total = 0
+    for member in group:
+        for a, b in side_checks:
+            if (vector.add(member, a) not in group
+                and (vector.add(member, b) not in group
+                     or vector.add(member, b, a) in group)):
+                total += 1
+    return total
 
 def search(rows):
-    groups = {}
-    pos_to_group = {}
-    edge = {(0, 0)}
-    while edge:
-        head = edge.pop()
-        this = grid.get(rows, head)
-        near_groups = set()
-        for d in grid.cardinals:
-            other_pos = vector.add(head, d)
+    seen = set()
+    group = set()
+    edge = set()
+    prio = {(0, 0)}
+    while prio or edge:
+        if not prio and group:
+            yield group
+            group = set()
+        head = (prio or edge).pop()
+        if head in seen:
+            continue
+        group.add(head)
+        seen.add(head)
+        val = grid.get(rows, head)
+        for cardinal in grid.cardinals:
+            other_pos = vector.add(head, cardinal)
             other = grid.get(rows, other_pos)
-            if this == other and other_pos in pos_to_group:
-                near_groups.add(groups[pos_to_group[other_pos]])
-            if other and other_pos not in pos_to_group:
-                edge.add(other_pos)
-        group = None
-        if near_groups:
-            group_iter = iter(near_groups)
-            group = next(group_iter)
-            for other in group_iter:
-                group.merge(groups, other)
-        else:
-            group = Group(len(groups), set())
-            groups[group.index] = group
-        pos_to_group[head] = group.index
-        group.members.add(head)
-    return set(groups.values())
+            if not other or other_pos in seen:
+                continue
+            (prio if val == other else edge).add(other_pos)
+    if group:
+        yield group
 
 def solve_p1(lines):
-    groups = search(lines)
-    return sum(group.area * group.border for group in groups)
+    return sum(len(group) * border(group) for group in search(lines))
 
 def solve_p2(lines):
-    groups = search(lines)
-    return sum(group.area * group.sides for group in groups)
+    return sum(len(group) * sides(group) for group in search(lines))
