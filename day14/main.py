c06ddd8a0995518ac9a0c1959f466ff04d71b94a
Adam Johnston <adam.johnston00@gmail.com>
2024-12-14 01:37:22 -0800

    faster day 14

--- day14/main.py
+++ day14/main.py
@@ -16,10 +16,7 @@ def parse(lines):
     return (w, h), robots
 
 def sim_steps(size, robot, steps):
-    xy, dxy = robot
-    for _ in range(steps):
-        xy = vector.posmod(vector.add(xy, dxy), size)
-    return xy
+    return vector.posmod(vector.add(robot[0], vector.mul(robot[1], steps)), size)
 
 def quad(size, xy):
     x = xy[0] / (size[0] - 1)
@@ -34,16 +31,31 @@ def solve_p1(lines):
     c.pop(None, None)
     return reduce(mul, c.values())
 
+def has_peaks(values, n=2, l=15):
+    values = Counter(values).most_common(2)
+    if values[0][1] < l or values[1][1] < l:
+        return False
+    return True
+
 def solve_p2(lines):
     size, robots = parse(lines)
     if size[0] < 50:
         return 'n/a'
+
     i, j = (size[0] - 1) // 2, (size[1] - 1) // 2
     check = {(ii, jj) for ii in range(i - 1, i + 2) for jj in range(j - 1, j + 2)}
+
+    peak_idxs = []
+    offset = 1
     steps = 0
     while True:
-        steps += 1
-        robots = [(sim_steps(size, robot, 1), robot[1]) for robot in robots]
+        steps += offset
+        robots = [(sim_steps(size, robot, offset), robot[1]) for robot in robots]
         posmap = {robot[0] for robot in robots}
+        if len(peak_idxs) < 2:
+            if has_peaks((xy[0] for xy in posmap)):
+                peak_idxs.append(steps)
+                if len(peak_idxs) == 2:
+                    offset = peak_idxs[1] - peak_idxs[0]
         if check <= posmap:
             return steps
