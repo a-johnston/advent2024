6a1f57df3d7a88755292fdb769d076d5e8a032d6
Adam Johnston <adam.johnston00@gmail.com>
2024-12-13 17:46:18 -0800

    shorter 13

--- day13/main.py
+++ day13/main.py
@@ -6,31 +6,25 @@ def parse_x_y(line):
     xs, ys = line.split(': ')[1].split(', ')
     return int(xs[2:]), int(ys[2:])
 
-def parse(lines, bonus=0):
+def parse(lines):
     it = iter(lines)
     for a in it:
         if not a:
             continue
-        a = parse_x_y(a)
-        b = parse_x_y(next(it))
-        target = vector.add(parse_x_y(next(it)), (bonus, bonus))
-        yield a, b, target
+        yield parse_x_y(a), parse_x_y(next(it)), parse_x_y(next(it))
 
-def tokens(a, b, c, limit=None):
-    x1, y1 = a
-    x2, y2 = b
-    det = x1 * y2 - y1 * x2
-    if det == 0:
-        return 0
-    x3, y3 = c
-    i = (y2 * x3 - x2 * y3) / det
-    j = (x1 * y3 - y1 * x3) / det
-    if i == int(i) and j == int(j):
-        return int(i) * 3 + int(j)
+def tokens(a, b, c, limit=float('inf'), bonus=0):
+    det = a[0] * b[1] - a[1] * b[0]
+    if det != 0:
+        c = vector.add(c, (bonus, bonus))
+        i = (b[1] * c[0] - b[0] * c[1]) / det
+        j = (a[0] * c[1] - a[1] * c[0]) / det
+        if all(x == int(x) and x < limit for x in (i, j)):
+            return int(i) * 3 + int(j)
     return 0
 
 def solve_p1(lines):
-    return sum(tokens(*vals, 100) for vals in parse(lines))
+    return sum(tokens(*vals, limit=100) for vals in parse(lines))
 
 def solve_p2(lines):
-    return sum(tokens(*vals) for vals in parse(lines, 10000000000000))
+    return sum(tokens(*vals, bonus=10 ** 13) for vals in parse(lines))
