a402dcd1c15994668cff8abee766ef723048316f
Adam Johnston <adam.johnston00@gmail.com>
2024-12-15 17:44:57 -0800

    day 15

---
+++ day15/main.py
@@ -0,0 +1,56 @@
+from adventlib import grid, util, vector
+
+dirs = {'>': grid.east, '<': grid.west, 'v': grid.south, '^': grid.north}
+reps = (('#', '##'), ('.', '..'), ('O', '[]'), ('@', '@.'))
+
+def parse(lines, wide):
+    rows, moves = util.split(lines)
+    if wide:
+        rows = [util.chain_replace(row, *reps) for row in rows]
+    robot = next(grid.indexes(rows, '@'))
+    walls = set(grid.indexes(rows, '#'))
+    blocks = {}
+    for block in ('O', '[', ']'):
+        blocks.update({xy: block for xy in grid.indexes(rows, block)})
+    return robot, blocks, walls, ''.join(moves)
+
+def merge(moves_a, moves_b):
+    if moves_a is None or moves_b is None:
+        return None
+    return {**moves_a, **moves_b}
+
+def get_block_moves(pos, blocks, walls, d):
+    if pos in walls:
+        return None
+    if pos not in blocks:
+        return dict()
+    moves = {pos: vector.add(pos, d)}
+    if blocks[pos] == '[':
+        right = vector.add(pos, grid.east)
+        moves[right] = vector.add(right, d)
+    elif blocks[pos] == ']' and d != grid.west:
+        left = vector.add(pos, grid.west)
+        moves[left] = vector.add(left, d)
+    for subpos in tuple(moves.values()):
+        if moves is not None and subpos not in moves:
+            moves = merge(moves, get_block_moves(subpos, blocks, walls, d))
+    return moves
+
+def do_move(robot, blocks, walls, d):
+    new_pos = vector.add(robot, d)
+    moves = get_block_moves(new_pos, blocks, walls, d)
+    if moves is None:
+        return robot
+    for start, end in sorted(moves.items(), key=lambda move: -vector.dot(move[1], d)):
+        blocks[end] = blocks[start]
+        blocks.pop(start)
+    return new_pos
+
+def helper(lines, wide):
+    robot, blocks, walls, moves = parse(lines, wide)
+    for move in moves:
+        robot = do_move(robot, blocks, walls, dirs[move])
+    return sum(j * 100 + i for i, j in blocks if blocks[(i, j)] != ']')
+
+solve_p1 = lambda lines: helper(lines, False)
+solve_p2 = lambda lines: helper(lines, True)
