af6f9438cae2c669875180ef092633a8837394d4
Adam Johnston <adam.johnston00@gmail.com>
2025-01-04 16:52:29 -0800

    slight change

--- day24/main.py
+++ day24/main.py
@@ -1,7 +1,7 @@
 from collections import defaultdict, namedtuple
 import operator
 
-Gate = namedtuple('Gate', ('op', 'args', 'out'))
+Gate = namedtuple('Gate', ('op', 'a', 'b', 'out'))
 
 operator_lookup = { 'XOR': operator.xor, 'AND': operator.and_, 'OR': operator.or_ }
 
@@ -14,11 +14,11 @@ def simulate(state, vars_to_gates):
             for gate in vars_to_gates[node]:
                 if gate.out in seen:
                     continue
-                if all(arg in state for arg in gate.args):
+                if gate.a in state and gate.b in state:
                     seen.add(gate.out)
                     new_assigned.add(gate.out)
-                    args = (state[arg] for arg in gate.args)
-                    state[gate.out] = operator_lookup[gate.op](*args)
+                    op = operator_lookup[gate.op]
+                    state[gate.out] = op(state[gate.a], state[gate.b])
         assigned = new_assigned
     total = 0
     for k in sorted((k for k in state if k[0] == 'z'), reverse=True):
@@ -26,37 +26,29 @@ def simulate(state, vars_to_gates):
     return total
 
 def parse(lines):
-    it = iter(lines)
-    vars_in = {}
+    state, gates = (x.split('\n') for x in '\n'.join(lines).split('\n\n'))
+    state = {k: int(v) for k, v in (s.split(': ') for s in state)}
     vars_to_gates = defaultdict(list)
-    outs_to_gate = {}
-    for line in it:
-        if not line:
-            break
-        idx, val = line.split(': ')
-        vars_in[idx] = int(val)
-    for line in it:
+    for line in gates:
         lh, out = line.split(' -> ')
         a, op, b = lh.split()
-        args = {a, b}
-        gate = Gate(op, args, out)
-        for arg in args:
-            vars_to_gates[arg].append(gate)
-        outs_to_gate[out] = gate
-    return vars_in, vars_to_gates, outs_to_gate
+        gate = Gate(op, a, b, out)
+        vars_to_gates[a].append(gate)
+        vars_to_gates[b].append(gate)
+    return state, vars_to_gates
 
 def solve_p1(lines):
-    return simulate(*parse(lines)[:2])
+    return simulate(*parse(lines))
 
 def solve_p2(lines):
-    _, __, otg = parse(lines)
-    if len(gates.state) < 90:
+    state, vars_to_gates = parse(lines)
+    otg = {gate.out: gate for gate in set.union(*map(set, vars_to_gates.values()))}
+    if len(state) < 90:
         return 'n/a'
-    otg = gates.outs_to_gate
     wrong = set()
     for gate in otg.values():
         if gate.op == 'XOR':
-            if any(x[0] in {'x', 'y'} for x in gate.args):
+            if gate.a not in otg or gate.b not in otg:
                 if gate.out[0] == 'z' and gate.out != 'z00':
                     wrong.add(gate.out)
             elif gate.out[0] != 'z':
@@ -66,12 +58,12 @@ def solve_p2(lines):
                 wrong.add(gate.out)
         else:
             if gate.op == 'OR':
-                for arg in gate.args:
+                for arg in (gate.a, gate.b):
                     if arg not in otg or otg[arg].op != 'AND':
                         wrong.add(arg)
             if gate.op == 'AND':
-                for arg in gate.args:
+                for arg in (gate.a, gate.b):
                     if arg in otg and otg[arg].op == 'AND':
-                        if 'x00' not in otg[arg].args:
+                        if 'x00' not in (otg[arg].a, otg[arg].b):
                             wrong.add(arg)
     return ','.join(sorted(wrong))
